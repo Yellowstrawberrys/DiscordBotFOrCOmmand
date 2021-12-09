37d3021f191a47c1249a6605afa173d9d5c1bd27
Yellowstrawberrys <dirjhhsg@gmail.com>
2021-12-09 01:02:02 +0100

    3번쨰 강의

---
+++ src/main/java/CommandHandlers/Ping.java
@@ -0,0 +1,25 @@
+package CommandHandlers;
+
+import cf.ystapi.jda.CommandHandler;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Ping implements CommandHandler {
+    @Override
+    public String name() {
+        return "ping";
+    }
+
+    @Override
+    public String helpMessages() {
+        return null;
+    }
+
+    @Override
+    public void onCalled(MessageReceivedEvent messageReceivedEvent, String[] args, MessageChannel Channel) {
+        long t = System.currentTimeMillis();
+        Channel.sendMessage("Pong!").queue(s -> {
+            s.editMessageFormat("Pong! \n time: %d", System.currentTimeMillis()-t).queue();
+        });
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import CommandHandlers.Ping;
+import cf.ystapi.jda.YSTBuilder;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder jdaBuilder = JDABuilder.createDefault("Token");
+        YSTBuilder ystBuilder = new YSTBuilder(jdaBuilder.build());
+        ystBuilder.setOwner("ur id")
+                .setPrefix("y!");
+
+        //Commands
+        ystBuilder.addCommand(new Ping());
+
+        //Build
+        ystBuilder.build();
+    }
+}
